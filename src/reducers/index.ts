299540b2d97d0be4950caf215d2ee15410982705
KurtZhou <pandabat1@gmail.com>
2020-04-29 12:17:46 +0800

    fix

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -20,19 +20,26 @@ export const reducer =(state=initState,action:any):IState =>{
         //改變輸入內容
         case CHANGE_TEXT:
             return {...state,input:{text:action.payload.input}}
+        
+        //增加選項
         case ADD_TODOLIST:
             const temp_ADD_Todo = state.todos.map(item=>item)
             const todoLength = temp_ADD_Todo.length
             temp_ADD_Todo.push({id:todoLength,toDoList:action.payload.todo,done:false})
             return {...state, todos: temp_ADD_Todo} 
+        
+        //完成工作
         case FINISH_TODOLIST:
             const temp_Finish_Todo = state.todos
             temp_Finish_Todo[action.payload.doneNo].done = !temp_Finish_Todo[action.payload.doneNo].done
             return {...state, todos:temp_Finish_Todo}
+        
+        //刪除選項
         case REMOVE_TODOLIST:
             const temp_remove_Todo = state.todos.map(item=>item)
             temp_remove_Todo.splice(action.payload.removeId,1)
             return {...state, todos:temp_remove_Todo}
+            
         default:
             return state
     }
