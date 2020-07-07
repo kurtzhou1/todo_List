a390c47097ee0fbebe36a5f78ed5b6dea981721d
KurtZhou <pandabat1@gmail.com>
2020-07-07 10:59:54 +0800

    add description

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+//創建Reducer後，還得將它交由store，store 的工作就是在應用程式中負責整合所有的Reducer。
+//創建前，得先從 redux 中 import 進負責創建 store 的函式 createStore ，並將 Reducer 傳入以創建一個 store：
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ToDoList from './components/ToDoList.tsx';
@@ -7,10 +10,14 @@ import {initState, reducer} from './reducers/index'
 
 
 const store = createStore(reducer,initState);
+//得先從 redux 中 import 進負責創建 store 的函式 createStore ，並將 Reducer 傳入以創建一個 store
+
 
 export const App=()=>{
     return(
         <Provider store={store}><ToDoList/></Provider>
+        //Provider是 react-redux中的組件，它會接收上方在Redux中創建的store
+        // 並根據和component綁在一起的需求單mapStateToProps上要求的資料從store中取出，再透過props流向component
     )
 }
 
--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,11 +1,16 @@
+// Reducer在Redux中是用來保管state
+// 以及在接收到不同的 action 指令時該對 state 做什麼動作的函數。
+
 import {CHANGE_TEXT,FINISH_TODOLIST,ADD_TODOLIST,REMOVE_TODOLIST} from '../constants'
 import {IState,todos} from '../common/lib'
 
 
+//在 initState 中將資料架構清楚列出，還能讓接手的人或是兩個禮拜後的自己，一看就能知道這個 Reducer 保管了哪些資料。
+
 export const initState:IState ={
     todos:[
         {id: 0 ,
-        toDoList: 'Hello~ 你好嗎，撞新幹線，珍重再見，臍帶再相逢',
+        toDoList: '12:00 大學同學會',
         done: true}
     ],
     input:{
@@ -13,8 +18,16 @@ export const initState:IState ={
     }
 }
 
-export const reducer =(state=initState,action:any):IState =>{
+//Reducer必須靠傳入action.type 的參數來判斷要執行什麼動作，以及執行此動作的額外參數
+//因此在執行事件時，得先設定傳入Reducer的物件：
 
+export const reducer =(state=initState,action:any):IState =>{
+    // Reducer本身是一個純函數
+
+
+
+    // 該物件有兩個key第一個type代表該執行的事件為何，進入Reducer時也會先判斷type的值
+    // payload則是執行事件的額外參數，上方的參數就是事件執行時要傳入的參數。
     switch(action.type){
         //改變輸入內容
         case CHANGE_TEXT:
